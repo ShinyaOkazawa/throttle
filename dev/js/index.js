b029f0ce66136c620d2dd7163e4ef4adab4424b0
Shinya Okazawa <fess3idm@gmail.com>
2016-08-20 18:34:01 +0900

    [modify]throttleの書き換え

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,44 +1,75 @@
+/*
+残タスク
+・コントローラーの作成
+・attachEventをaddEventListenerにbindする
+→addEventListenerだけでattachEventをカバーできるようにする
+*/
+
+// polyfill
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 var ZAWA = ZAWA || {};
-(function(global){
+(function(){
 
-	ZAWA.throttle = (function(){
-		var p = Throttle.prototype;
+	ZAWA.Throttle = (function(){
 
-		function Throttle(time, func){
-			this._lastTime = 0;
-			this._interval = (time) ? time : 1000;
-			this._callback = (func) ? func : undefined;
+		class Throttle {
+			constructor(interval, callback){
+				this._interval = interval;
+				this._lastTime = 0;
+				this._callback = callback;
 
-			this._init();
-		}
-
-		p._init = function(){
-			this._lastTime = new Date().getTime() - this._interval;
-
-			if(window.addEventListener){
-				window.addEventListener('resize', this._throttle.bind(this), false);
-			} else if(window.attachEvent){
-				window.attachEvent('onresize', this._throttle.bind(this));
+				this._init();
 			}
-		};
 
-		p._throttle = function(){
-			if((this._lastTime + this._interval) <= new Date().getTime()){
-				this._lastTime = new Date().getTime();
-				if(this._callback){
+			_init () {
+				this._lastTime = new Date().getTime() - this._interval;
+			}
+
+			run () {
+				if((this._lastTime + this._interval) <= new Date().getTime()){
+					this._lastTime = new Date().getTime();
 					this._callback();
+					return;
 				}
 			}
-		};
+		}
 
 		return Throttle;
 	}());
 
-	global.ZAWA = ZAWA;
-}(window));
+}());
 
+var throttle = new ZAWA.Throttle(500, hello);
 
-new ZAWA.throttle(500,hello);
+if(window.addEventListener){
+	window.addEventListener('resize', () => { throttle.run(); }, false);
+} else if(window.attachEvent){
+	window.attachEvent('onresize', () => { throttle.run(); });
+}
 
 function hello(){
 	console.log('hello');
--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -1,45 +1,88 @@
-'use strict';
+"use strict";
 
-var ZAWA = ZAWA || {};
-(function (global) {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	ZAWA.throttle = function () {
-		var p = Throttle.prototype;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-		function Throttle(time, func) {
-			this._lastTime = 0;
-			this._interval = time ? time : 1000;
-			this._callback = func ? func : undefined;
+/*
+残タスク
+・コントローラーの作成
+・attachEventをaddEventListenerにbindする
+→addEventListenerだけでattachEventをカバーできるようにする
+*/
 
-			this._init();
+// polyfill
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function (oThis) {
+		if (typeof this !== "function") {
+			// closest thing possible to the ECMAScript 5
+			// internal IsCallable function
+			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
 		}
 
-		p._init = function () {
-			this._lastTime = new Date().getTime() - this._interval;
-
-			if (window.addEventListener) {
-				window.addEventListener('resize', this._throttle.bind(this), false);
-			} else if (window.attachEvent) {
-				window.attachEvent('onresize', this._throttle.bind(this));
-			}
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+		    fToBind = this,
+		    fNOP = function fNOP() {},
+		    fBound = function fBound() {
+			return fToBind.apply(this instanceof fNOP && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
 		};
 
-		p._throttle = function () {
-			if (this._lastTime + this._interval <= new Date().getTime()) {
-				this._lastTime = new Date().getTime();
-				if (this._callback) {
-					this._callback();
+		fNOP.prototype = this.prototype;
+		fBound.prototype = new fNOP();
+
+		return fBound;
+	};
+}
+
+var ZAWA = ZAWA || {};
+(function () {
+
+	ZAWA.Throttle = function () {
+		var Throttle = function () {
+			function Throttle(interval, callback) {
+				_classCallCheck(this, Throttle);
+
+				this._interval = interval;
+				this._lastTime = 0;
+				this._callback = callback;
+
+				this._init();
+			}
+
+			_createClass(Throttle, [{
+				key: "_init",
+				value: function _init() {
+					this._lastTime = new Date().getTime() - this._interval;
 				}
-			}
-		};
+			}, {
+				key: "run",
+				value: function run() {
+					if (this._lastTime + this._interval <= new Date().getTime()) {
+						this._lastTime = new Date().getTime();
+						this._callback();
+						return;
+					}
+				}
+			}]);
+
+			return Throttle;
+		}();
 
 		return Throttle;
 	}();
+})();
 
-	global.ZAWA = ZAWA;
-})(window);
+var throttle = new ZAWA.Throttle(500, hello);
 
-new ZAWA.throttle(500, hello);
+if (window.addEventListener) {
+	window.addEventListener('resize', function () {
+		throttle.run();
+	}, false);
+} else if (window.attachEvent) {
+	window.attachEvent('onresize', function () {
+		throttle.run();
+	});
+}
 
 function hello() {
 	console.log('hello');
